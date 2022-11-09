122a55f48ef745fd4a886e810b640988516ea97d
David <davidtolosa5@gmail.com>
2022-11-08 22:23:32 -0300

    Stragegy

---
+++ src/comportamiento/strategy/Main.java
@@ -0,0 +1,12 @@
+package comportamiento.strategy;
+
+public class Main {
+    public static void main(String[] args) {
+        VistaCatalogo vistaCatalogo1 = new VistaCatalogo(new DibujaTresVehiculosPorLinea());
+        vistaCatalogo1.dibuja();
+        VistaCatalogo vistaCatalogo2 = new VistaCatalogo(new DibujaUnVehiculoPorLinea());
+        vistaCatalogo2.dibuja();
+        VistaCatalogo vistaCatalogo3 = new VistaCatalogo(new DibujaCuatroVehiculosPorLinea());
+        vistaCatalogo3.dibuja();
+    }
+}
